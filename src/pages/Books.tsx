6619f5b98fc553a6af6f2b6ed783ea94c5a9363d
samssjubair <samssjubair@gmail.com>
2023-07-16 22:18:33 +0600

    filtering and search in book page

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,16 +1,38 @@
 import { useState } from "react";
 import { useGetBooksQuery } from "../redux/features/books/bookApi";
+// import { useHistory } from "react-router-dom";
 
 const Books = () => {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [searchText, setSearchText] = useState("");
-    const { data, isLoading, error } = useGetBooksQuery({searchTerm});
-    const handleSearch = () => {
-      setSearchTerm(searchText);
-      
-    };
-    return (
-      <div>
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [filterGenre, setFilterGenre] = useState("");
+  const [filterYear, setFilterYear] = useState("");
+  const { data, isLoading, error } = useGetBooksQuery({
+    searchTerm,
+    genre: filterGenre,
+    year: filterYear,
+  });
+
+  const handleSearch = () => {
+    setSearchTerm(searchText);
+  };
+
+  const handleFilterGenre = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterGenre(e.target.value);
+  };
+
+  const handleFilterYear = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterYear(e.target.value);
+  };
+
+  const handleAddNew = () => {
+    // history.push("/add-new-book");
+    console.log("add new");
+  };
+
+  return (
+    <div>
+      <div className="flex justify-between">
         <div className="flex m-4">
           <input
             type="text"
@@ -26,46 +48,79 @@ const Books = () => {
             Search
           </button>
         </div>
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Title
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Author
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Genre
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Publication Year
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {data?.data.map((book) => (
-              <tr key={book._id}>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <div className="text-sm text-gray-900">{book.title}</div>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <div className="text-sm text-gray-900">{book.author}</div>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <div className="text-sm text-gray-900">{book.genre}</div>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <div className="text-sm text-gray-900">
-                    {book.publicationYear}
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div className="flex m-4">
+          <label htmlFor="genre-select" className="mr-2">
+            Genre:
+          </label>
+          <select
+            id="genre-select"
+            value={filterGenre}
+            onChange={handleFilterGenre}
+            className="px-4 py-2 border border-gray-300 rounded-md"
+          >
+            <option value="">All</option>
+            <option value="fiction">Fiction</option>
+            <option value="non-fiction">Non-Fiction</option>
+            <option value="comic">Comic</option>
+          </select>
+          <label htmlFor="year-input" className="ml-4 mr-2">
+            Year:
+          </label>
+          <input
+            type="text"
+            value={filterYear}
+            onChange={handleFilterYear}
+            placeholder="Filter by year..."
+            className="px-4 py-2 border border-gray-300 rounded-md"
+          />
+        </div>
       </div>
-    );
+      <button
+        onClick={handleAddNew}
+        className="m-4 px-4 py-2 bg-green-500 text-white rounded-md"
+      >
+        Add New
+      </button>
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Title
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Author
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Genre
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Publication Year
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {data?.data.map((book) => (
+            <tr key={book._id}>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="text-sm text-gray-900">{book.title}</div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="text-sm text-gray-900">{book.author}</div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="text-sm text-gray-900">{book.genre}</div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="text-sm text-gray-900">
+                  {book.publicationYear}
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Books;
--- src/redux/features/books/bookApi.ts
+++ src/redux/features/books/bookApi.ts
@@ -6,14 +6,16 @@ const bookApi = api.injectEndpoints({
       query: (id) => `/books/${id}`,
     }),
     getBooks: builder.query({
-      query: ({ page, limit, sort, searchTerm }) => {
-        let queryString = `/books?searchTerm=${searchTerm}`;
+      query: ({ page, limit, sort, searchTerm="", year="",genre="" }) => {
+        let queryString = `/books`;
 
         if (page && limit && sort) {
           queryString += `?page=${page}&limit=${limit}&sort=-${sort}`;
         }
 
-
+        if (searchTerm || year || genre) {
+            queryString += `?searchTerm=${searchTerm}&publicationYear=${year}&genre=${genre}`;
+        }
 
         return queryString;
       },
